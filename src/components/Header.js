08cc3d8fc2887dc65eb80525068f4519c07b7c0f
AnshKaushal <ansh1043.be21@chitkarauniversity.edu.in>
2022-05-22 10:17:00 +0530

    ssws

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Header = ({ handleToggleDarkMode }) => {
 	return (
 		<div className='header'>
-			<h1>Notes</h1>
+			<h1>Confessions</h1>
+			<p>Please don't delete any note</p>
 			<button
 				onClick={() =>
 					handleToggleDarkMode(
